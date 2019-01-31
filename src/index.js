14cad8b1e87c0ef16ba994c89da725120d754c1a
Jack Jain <cirocelot@gmail.com>
2019-01-31 13:03:27 -0700

    created canvas in js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ var canvasContext;
 
 window.onload = function(){
 	console.log("Sup World!");
-	canvas = document.getElementById('gameScreen')
+	canvas = document.getElementById("gameScreen");
+	canvasContext = canvas.getContext("2d");
+	canvasContext.fillStyle = "black";
+	canvasContext.fillRect(0,0, canvas.width, canvas.height);
 };
